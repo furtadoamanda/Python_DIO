fb24e7349fccc3ed7004dc7e4ec233fa14798106
Amanda Furtado <111905791+furtadoamanda@users.noreply.github.com>
2024-04-09 15:35:11 -0300

    Tipos de Operadores

---
+++ Tipos de Operadores com Python/operadores_associacao.py
@@ -0,0 +1,11 @@
+frutas = ["limao", "uva", "laranja"]
+
+print("laranja" in frutas)
+print("maçã" in frutas)
+print("uva" not in frutas)
+print("Laranja" in frutas)
+
+curso = "Curso de Python"
+
+print("Python" in curso)
+print("python" not in curso)
---
+++ Tipos de Operadores com Python/operadores_comparacao.py
@@ -0,0 +1,9 @@
+saldo = 200
+saque = 300
+
+print(saldo == saque)
+print(saldo != saque)
+print(saldo > saque)
+print(saldo >= saque)
+print(saldo < saque)
+print(saldo <= saque)
