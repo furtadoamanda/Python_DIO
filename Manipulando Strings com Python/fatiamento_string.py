b71e66612f3eed293edc2f65c955f0509dab7943
Amanda <furtadodesouzaamanda@gmail.com>
2024-04-10 17:32:40 -0300

    Manipulando Strings com Python 2

---
+++ Manipulando Strings com Python/fatiamento_string.py
@@ -0,0 +1,18 @@
+nome = "Guilherme Arthur de Carvalho"
+
+print(nome[0]) # índice 0
+
+print(nome[-1]) # último item
+
+print(nome[:9]) # até o 9
+
+print(nome[10:]) # a partir do 10
+
+print(nome[10:16]) # do 10 ao 16
+
+print(nome[10:16:2]) # pulando de 2 em 2
+
+print(nome[ : ]) # string inteira
+
+print(nome[::-1]) # espelhamento
+
---
+++ Manipulando Strings com Python/string_multilinhas.py
@@ -0,0 +1,26 @@
+nome = "Guilherme"
+
+mensagem = f"""
+Olá meu nome é {nome},
+Eu estou aprendendo Python
+"""
+
+mensagem2 = f'''
+    Olá meu nome é {nome},
+ Eu estou aprendendo Python.
+      Essa mensagem tem diferentes recuos.
+'''
+
+print(mensagem + mensagem2)
+
+print('''
+    ****************** MENU ********************
+      
+      1- Depositar
+      2- Sacar
+      0- Sair
+
+    ********************************************
+
+        Obrigado por usar esse sistema!!
+''')
